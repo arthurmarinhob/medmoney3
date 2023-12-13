c0abe569927321338b1b466b215caf43ab3e0880
arturito21 <arthurmarinhob21@gmail.com>
2023-12-13 18:50:13 -0300

    resolvendo os erros de compilação, e tentando colocar para rodar no celular

--- App.js
+++ App.js
@@ -1,5 +1,7 @@
 import React, { useState } from 'react';
-import { View, TextInput, Button, FlatList, Text, StyleSheet, Switch } from 'react-native';
+import { View, TextInput, Button, FlatList, Text, StyleSheet, Switch, TouchableOpacity, Alert, Modal } from 'react-native';
+import DropDownMenu from 'react-native-dropdown-menu';
+import DateTimePicker from '@react-native-community/datetimepicker';
 
 export default function App() {
   const [todosLancamentos, setTodosLancamentos] = useState([]);
@@ -14,13 +16,21 @@ export default function App() {
 
   const [filtroInativos, setFiltroInativos] = useState(false);
 
+  const [observacoesModalVisible, setObservacoesModalVisible] = useState(false);
+
+  const [observacoesTemp, setObservacoesTemp] = useState('');
+
+  const [observacoesIndex, setObservacoesIndex] = useState(null); // Índice do item com observações
+  
+  const [listaLancamentos, setListaLancamentos] = useState([]);
+
+  const [isDatePickerVisible, setDatePickerVisible] = useState(false);
+
   const itensFiltrados = todosLancamentos.filter((item) => {
-    const ativo = filtroAtivos ? item.check : true;
     const inativo = filtroInativos ? !item.check : true;
     
-    return ativo && inativo;
+    return inativo;
   });
-
 
   const handleChange = (campo, valor) => {
     setNovoLancamento({
@@ -47,6 +57,71 @@ export default function App() {
     setTodosLancamentos(updatedLancamentos);
   };
 
+  const handleRemoveItem = (index) => {
+    const updatedLancamentos = [...todosLancamentos];
+    updatedLancamentos.splice(index, 1); // Remove o item do array
+    setTodosLancamentos(updatedLancamentos);
+  };
+
+  const [selectedIndex, setSelectedIndex] = useState(null);
+
+  const handleLongPress = (index) => {
+    // Quando um item é pressionado e mantido, exibe o menu de contexto nativo
+    Alert.alert(
+      'Opções',
+      'Escolha uma opção:',
+      [
+        {
+          text: 'Observações',
+          onPress: () => handleAddObservations(index),
+        },
+        {
+          text: 'Remover',
+          onPress: () => handleRemoveItem(index),
+        },
+        {
+          text: 'Cancelar',
+          style: 'cancel',
+        },
+      ],
+    );
+  };
+
+  const handleAddObservations = (index) => {
+    setObservacoesIndex(index); // Armazena o índice do item com observações
+    setObservacoesTemp(itensFiltrados[index]?.obs || ''); // Carrega as observações existentes
+    setObservacoesModalVisible(true);
+  };
+
+  const saveObservacoes = () => {
+    if (observacoesIndex !== null) {
+      const updatedLancamentos = [...todosLancamentos];
+      updatedLancamentos[observacoesIndex].obs = observacoesTemp;
+      setTodosLancamentos(updatedLancamentos);
+    }
+    setObservacoesTemp('');
+    setObservacoesModalVisible(false);
+  };
+
+  const showDatePicker = () => {
+    setDatePickerVisible(true);
+  };
+  
+  const handleDateChange = (event, selectedDate) => {
+    setDatePickerVisible(false);
+  
+    if (selectedDate) {
+      handleChange('data', selectedDate);
+    }
+  };
+  
+  const formatDate = (date) => {
+    if (!date) return '';
+    const formattedDate = new Date(date).toLocaleDateString();
+    return formattedDate;
+  };
+  
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleContainer}>Meus plantões</Text>
@@ -56,7 +131,7 @@ export default function App() {
           value={filtroInativos}
           onValueChange={(value) => setFiltroInativos(value)}
         />
-        <Text>Caloreiros</Text>
+        <Text>Mostrar só os caloteiros? </Text>
       </View>
 
       <View style={styles.headerContainer}>
@@ -72,30 +147,41 @@ export default function App() {
         data={itensFiltrados}
         keyExtractor={(item, index) => index.toString()}
         renderItem={({ item, index }) => (
-          <View style={styles.lancamentoItem}>
-            <Text style={styles.label}>{item.data}</Text>
-            <Text style={styles.label}>{item.tipo}</Text>
-            <Text style={styles.label}>{item.nome}</Text>
-            <Text style={styles.label}>{item.valor}</Text>
-            <Text style={styles.label}>{item.obs}</Text>
+          <TouchableOpacity onLongPress={() => handleLongPress(index)}>
+            <View style={styles.lancamentoItem}>
+              <Text style={styles.label}>{item.data}</Text>
+              <Text style={styles.label}>{item.tipo}</Text>
+              <Text style={styles.label}>{item.nome}</Text>
+              <Text style={styles.label}>{item.valor}</Text>
+              <Text>{item.obs ? <Button title="Obs" onPress={() => handleAddObservations(index)} /> : ''}</Text>
 
-            <View style={styles.switchContainer}>
-              <Switch
-                value={item.check}
-                onValueChange={(value) => handleSwitchChange(index, value)}
-              />
+              <View style={styles.switchContainer}>
+                <Switch
+                  value={item.check}
+                  onValueChange={(value) => handleSwitchChange(index, value)}
+                />
+              </View>
+
             </View>
-          </View>
+          </TouchableOpacity>
         )}
       />
 
       <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Data"
-          value={novoLancamento.data}
-          onChangeText={(text) => handleChange('data', text)}
-          style={styles.input}
-        />
+        <TouchableOpacity onPress={showDatePicker}>
+          <View style={styles.datePickerContainer}>
+            <Text>Data: {formatDate(novoLancamento.data)}</Text>
+          </View>
+        </TouchableOpacity>
+
+        {isDatePickerVisible && (
+          <DateTimePicker
+            value={novoLancamento.data || new Date()} // Defina a data atual ou a data existente
+            mode="date"
+            display="default"
+            onChange={handleDateChange}
+          />
+        )}
         <TextInput
           placeholder="Tipo"
           value={novoLancamento.tipo}
@@ -122,7 +208,36 @@ export default function App() {
         />
       </View>
 
-      <Button title="Enviar" onPress={handleSubmit} />
+      <View style={{ flexDirection: 'row-reverse', marginLeft: 20 }}>
+        <Button  title="Enviar" onPress={handleSubmit} />
+      </View>
+
+      {/* Modal de Observações */}
+      <Modal
+        animationType="slide"
+        transparent={true}
+        visible={observacoesModalVisible}
+        onRequestClose={() => {
+          setObservacoesModalVisible(false);
+        }}
+      >
+        <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+          <View style={{ backgroundColor: 'white', padding: 20, borderRadius: 10, elevation: 5 }}>
+            <Text>Observações</Text>
+            <TextInput
+              placeholder="Observações"
+              value={observacoesTemp}
+              onChangeText={(text) => setObservacoesTemp(text)}
+              style={styles.input}
+            />
+            <Button title="Salvar" onPress={saveObservacoes} />
+            <TouchableOpacity onPress={() => setObservacoesModalVisible(false)}>
+              <Text>Fechar</Text>
+            </TouchableOpacity>
+          </View>
+        </View>
+      </Modal>
+
     </View>
   );
 }
@@ -132,6 +247,7 @@ const styles = StyleSheet.create({
     flex: 1,
     padding: 10,
     marginTop: 60,
+    marginBottom: 40
   },
   titleContainer: {
     textAlign: 'center',
@@ -172,10 +288,6 @@ const styles = StyleSheet.create({
     padding: 10,
     marginVertical: 5,
   },
-  label: {
-    flex: 1, // Faz com que os itens ocupem espaço igual
-    marginRight: 5, // Adiciona margem direita para separar os itens
-  },
   lancamentoItem: {
     flexDirection: 'row',
     justifyContent: 'space-between',
@@ -196,4 +308,11 @@ const styles = StyleSheet.create({
     marginRight: 5, // Adiciona margem direita para separar os itens
     textAlign: 'center',
   },
+  datePickerContainer: {
+    borderWidth: 1,
+    borderColor: 'gray',
+    borderRadius: 5,
+    padding: 10,
+    marginBottom: 10,
+  },  
 });
